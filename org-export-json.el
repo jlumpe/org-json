72a1b9932698b292ae73a1d829f239c7fffa7e8c
Jared Lumpe <mjlumpe@gmail.com>
2019-02-19 17:18:13 -0800

    ELPA package stuff?

--- org-export-json.el
+++ org-export-json.el
@@ -1,4 +1,29 @@
-;; -*- lexical-binding: t -*-
+;;; org-export-json.el --- export org-mode files as JSON          -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Jared Lumpe
+
+;; Author: Jared Lumpe
+;; Keywords: outlines
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 
 (require 'org)
 (require 'json)
@@ -176,3 +201,5 @@
 
 
 (provide 'org-export-json)
+
+;;; org-export-json.el ends here
